f6c6c4b1047131009cdeeedf6febdf21467f1431
Practicalli Engineering <engineering@practical.li>
2026-02-15 21:09:56 +0000

    debian: practicalli configuration files and content

---
+++ debian-linux/practicalli-config-and-content.sh
@@ -0,0 +1,52 @@
+# -----------------------------------------------
+# Practicalli Configuration for development environment
+
+# NOTE: dotfiles repo contains these scripts, so should already be downloaded
+# Although maybe download as a zip file (or release)
+# and this script clones the repository to maintain the config and push updates
+
+
+# echo "# ---------------------------------------"
+# echo "Practicalli Configuration"
+# echo "# ---------------------------------------"
+
+# echo ""
+
+# Configure system
+
+# Create directories for Practicalli projects
+# Check they exist
+# $HOME/projects/practicalli/books
+# $HOME/projects/practicalli/books-new
+
+# GitHub CLI login
+# gh auth login
+
+# echo "# ---------------------------------------"
+# echo "Install Practicalli Dotfiles"
+#
+# echo "# ---------------------------------------"
+
+# gh repo clone practicalli/dotfiles ~/projects/practicalli/ --depth 1
+
+# echo "# ---------------------------------------"
+# echo "Configure Git Client"
+# echo "# ---------------------------------------"
+
+# echo "# ---------------------------------------"
+# echo "Configure Zsh"
+# echo "# ---------------------------------------"
+
+# echo "# ---------------------------------------"
+# echo "Install Clojure CLI Configuration"
+# echo "# ---------------------------------------"
+
+
+# echo ""
+# echo ""
+#
+# echo "# ---------------------------------------"
+# echo "Clone Practicalli Books"
+# echo "# ---------------------------------------"
+
+# gh repo clone practicalli/cycling ~/projects/practicalli/books-new/cycling --depth 1
