45632d740500c6b5553362486fddd61d5f51ef1c
Practicalli Engineering <engineering@practical.li>
2026-02-17 14:14:40 +0000

    debian: example rust via rustup install
    Shell script requires some testing.

---
+++ debian-linux/rust-rustup-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# TODO: to test
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source "$HOME"/.cargo/env
+
+BASH_COMPLETION_DIR="$HOME"/.local/share/bash-completion/completions
+ZSH_COMPLETION_DIR="$HOME/.local/share/zsh-completion"
+
+# bash
+rustup completions bash rustup > "$BASH_COMPLETION_DIR"/rustup
+rustup completions bash cargo > "$BASH_COMPLETION_DIR"/cargo
+
+# zsh
+rustup completions zsh rustup > "$ZSH_COMPLETION_DIR"/_rustup
+rustup completions zsh cargo > "$ZSH_COMPLETION_DIR"/_cargo
