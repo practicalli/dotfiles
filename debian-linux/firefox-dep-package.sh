e0a4566ded99cb9916efa817930f7fc337a972ee
Practicalli Engineering <engineering@practical.li>
2026-02-17 14:12:50 +0000

    debian: firefox install from debian package
    Removes snap install if it exists.
    
    More relevant to Ubuntu than Debian Linux, but useful to have anyway.

---
+++ debian-linux/firefox-dep-package.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# This script install firefox as .deb package from mozilla ppa, replacing ubuntu snap version
+# Source:
+# - https://support.mozilla.org/en-US/kb/install-firefox-linux#w_install-firefox-deb-package-for-debian-based-distributions-recommended
+# - https://askubuntu.com/a/1516224
+
+sudo snap remove --purge firefox
+
+# 1. Create a directory to store APT repository keys if it doesn't exist
+sudo install -d -m 0755 /etc/apt/keyrings
+
+# 2. Import the Mozilla APT repository signing key
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+# 3. The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+
+# 4. Next, add the Mozilla APT repository to your sources list
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+# 5. Configure APT to prioritize packages from the Mozilla repository
+echo '
+Package: firefox*
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla > /dev/null
+
+echo 'Unattended-Upgrade::Origins-Pattern { "archive=mozilla"; };' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+# 6. Update your package list, and install the Firefox .deb package
+sudo apt-get update && sudo apt-get install -y --allow-downgrades firefox
+
+
+# -----------------------------------------------
+# Use developer edition of Firefox instead
+
+# if [[ ! -e /etc/apt/sources.list.d/mozilla.list ]]; then
+#   echo "Mozilla APT repository is not set. You should first run 'firefox-deb' script"
+#   exit 1
+# fi
+#
+# sudo apt install -y firefox-devedition
+# -----------------------------------------------
