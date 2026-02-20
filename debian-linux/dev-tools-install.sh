9d185d3fdf8a11c6b9ca88fcfc652d71a7eda13c
Practicalli Engineering <engineering@practical.li>
2026-02-20 01:32:58 +0000

    debian: specify appimage for neovim install
    DRA only installs the node binary and not the node-runtime when using automatic mode.
    
    Assumption: Dra is downloading the tar.gz archive and extracting the nvim file
    
    Specify `nvim-linux-x86_64.appimage` as the selection patten directs DRA to download the appimage and instal in the given directory
    
    A symbolic link is created so Neovim can be called with the standard `nvim` command name

--- debian-linux/dev-tools-install.sh
+++ debian-linux/dev-tools-install.sh
@@ -45,7 +45,13 @@ echo "# ---------------------------------------"
 echo "Neovim hyper-configurable editor - installed for all users in `/usr/local/bin`"
 # remove the existing nvim binary as DRA fails to override the file
 # sudo rm -f /usr/local/bin/nvim
-sudo dra download --automatic --install --output /usr/local/bin neovim/neovim
+
+# install the nvim.appimage (automatic only installs nvim and not runtime)
+sudo dra download --select nvim-linux-x86_64.appimage --install --output /usr/local/bin neovim/neovim
+
+# Create symbolic link called nvim
+sudo ln -s /usr/local/bin/nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
 echo "# ---------------------------------------"
 
 echo ""
