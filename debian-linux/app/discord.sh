64afbfbf23ae08d23f24831fb809de1ae119e478
Practicalli Engineering <engineering@practical.li>
2026-02-22 21:00:48 +0000

    debian: install discord app script
    Download the official Discord deb package and install using apt-get

---
+++ debian-linux/app/discord.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# 1Password Desktop app for password management
+# https://support.1password.com/install-linux/
+
+echo
+echo "# ---------------------------------------"
+echo "Install Discord via Debian Linux package system"
+echo
+
+
+echo
+echo "Download official Discord Debian Package"
+curl --location "https://discordapp.com/api/download?platform=linux&format=deb" --output /tmp/discord-latest.deb
+echo
+
+echo
+echo "Install Discord package"
+sudo apt-get install /tmp/discord-latest.deb
+echo
+
+echo
+echo "Discord App now available via desktop launcher and command line:"
+which discord
+echo "# ---------------------------------------"
+echo
