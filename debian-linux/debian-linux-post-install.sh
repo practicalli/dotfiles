c320fdc41e78aa10bffd0e314960141754b17b4d
Practicalli Engineering <engineering@practical.li>
2026-04-08 13:20:34 +0100

    debian: format & simplify debian post install script

--- debian-linux/debian-linux-post-install.sh
+++ debian-linux/debian-linux-post-install.sh
@@ -12,54 +12,38 @@ purge="debian-linux-post-install-packages-purge.list"
 
 ## Update available Debian packages
 echo
-echo "# ---------------------------------------"
 echo Update available Debian packages
 sudo apt update
-echo "# ---------------------------------------"
 echo
 
 # Install additional Debian packages
-while read -r line
-do
-  echo "# ---------------------------------------"
+while read -r line; do
   echo Install "$line"
   sudo apt-get --yes --ignore-missing install "$line"
-  echo "# ---------------------------------------"
-done < "$add"
+done <"$add"
 echo
 
-echo "# ---------------------------------------"
 echo Install System Clipboard tool - X11 or Wayland
 ./clipboard.sh
-echo "# ---------------------------------------"
 echo
 
-echo "# ---------------------------------------"
 echo Configure Kitty as default terminal
 sudo update-alternatives --set x-terminal-emulator "/usr/bin/kitty"
-echo "# ---------------------------------------"
 echo
 
 # Remove additional Debian packages
-while read -r line
-do
+while read -r line; do
   echo
-  echo "# ---------------------------------------"
   echo Purge "$line"
-  echo "# ---------------------------------------"
   echo
   sudo apt-get purge --yes --ignore-missing "$line"
-done < "$purge"
+done <"$purge"
 echo
 
-echo "# ---------------------------------------"
 echo Uninstall unnecessary Debian packages
 sudo apt-get autopurge
-echo "# ---------------------------------------"
 echo
 
-echo "# ---------------------------------------"
 echo Remove Debian package files from cache
 sudo apt-get clean
-echo "# ---------------------------------------"
 echo
