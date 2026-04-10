732734255f7429fc9a1c28542bf14d792cd7ef43
Practicalli Engineering <engineering@practical.li>
2026-04-10 19:18:13 +0100

    debian: check for git client to download practicalli dotfiles

--- debian-linux/practicalli/install-configs.sh
+++ debian-linux/practicalli/install-configs.sh
@@ -14,11 +14,25 @@ echo "Install Practicalli Dotfiles"
 
 if [ ! -d "$HOME"/projects/practicalli/dotfiles ]; then
 
-  echo "GitHub CLI login"
-  gh auth login
+  # Check for Git Client
+  if [ -d "$HOME"/.ssh] && [command -v "git" > /dev/null 2>&1]; then
+    echo "Using Git client & SSH to clone Practicalli Dotfiles repository"
+    git clone --depth=1 git@github.com:practicalli/dotfiles ~/projects/practicalli/dotfiles
+  elif [ command -v "gh" > /dev/null 2>&1 ]; then
+    # echo "GitHub CLI login"
+    # gh auth login
 
-  echo "Clone Practicalli Dotfiles repository"
-  gh repo clone practicalli/dotfiles ~/projects/practicalli/ --depth 1
+    echo "Using GitHub CLI to clone Practicalli Dotfiles repository"
+    gh repo clone practicalli/dotfiles ~/projects/practicalli/dotfiles -- --depth=1
+  else
+    # Download the zip and extract
+    echo "Downloading Zip file of Practicalli Dotfiles repository"
+    curl --fail --location --silent --show-error --output /tmp/practicalli-dotfiles.zip "https://github.com/practicalli/dotfiles/archive/refs/heads/main.zip"
+    unzip /tmp/practicalli-dotfiles.zip -d "$HOME/projects/practicalli/dotfiles"
+    echo
+    echo "Practicalli Dotfiles installed at: $HOME/projects/practicalli/dotfiles"
+  fi
+
 else
   echo "Practicalli Dotfiles already installed"
 fi
