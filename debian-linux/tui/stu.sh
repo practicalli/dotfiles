ba3b60ddf6159a368738120b294cd8535465b50e
Practicalli Engineering <engineering@practical.li>
2026-04-18 21:26:31 +0100

    debian: stu tui to view aws s3 buckets

---
+++ debian-linux/tui/stu.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "# ---------------------------------------"
+echo "STU - browse AWS S3 buckets and objects"
+dra download --automatic --install --output ~/.local/bin/ lusingander/stu
+
+echo
+echo "STU version: $(stu --version)"
+echo "# ---------------------------------------"
+echo
