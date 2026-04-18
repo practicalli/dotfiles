ce17f32268fed2549a755f8fd6c4248e893f4f4a
Practicalli Engineering <engineering@practical.li>
2026-04-18 21:42:06 +0100

    debian: blippy tui to manage github issues and pull requests

---
+++ debian-linux/tui/blippy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "# ---------------------------------------"
+echo "Blippy - manage GitHub Issues and Pull Requests"
+dra download --automatic --install --output ~/.local/bin/ AksharP5/blippy
+
+echo
+echo "Blippy version: $(blippy --version)"
+echo "# ---------------------------------------"
+echo
