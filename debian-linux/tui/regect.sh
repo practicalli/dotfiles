6e6bed7a0ced4dafb2fa33c0da877a92ab8c79e3
Practicalli Engineering <engineering@practical.li>
2026-04-18 20:48:43 +0100

    debian: regect tui explains regular expressions

---
+++ debian-linux/tui/regect.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo
+echo "# ---------------------------------------"
+echo "Regect - explain regular expressions"
+dra download --automatic --install --output ~/.local/bin/ kloki/regect
+
+# WARN: regect does not have a version flag
+# echo
+# echo "Regect version: $(regect --version)"
+echo "# ---------------------------------------"
+echo
